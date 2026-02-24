ce24a090b368ba0663c74a924d7e87c394d8607c
unknown <clydemail.inc@gmail.com>
2026-02-24 01:37:57 -0500

    added comments to 2nd problem and finished question 3

--- Question2.java
+++ Question2.java
@@ -2,27 +2,26 @@
 public class Question2{
 
   public static int strTraversal(String inString, char inChar){
-
+//returns if end of string/ ends recursion
      if(inString == null || inString.isEmpty() || inString.equals("") ){
-
       return 0;
+//checks if first letter in string is == to char then adds to total and continues
     }else if(inString.charAt(0) == inChar){
       return 1 + strTraversal(inString.substring(1), inChar);
     }
     else{
+//continues recursion but doesnt add to total
       return strTraversal(inString.substring(1), inChar);
     }
 
   }
 
   public static void main(String[] args){
-
+//takes first string and only first char of second string
     long startTime = System.nanoTime();
-
     int totalChar = strTraversal(args[0],args[1].toCharArray()[0]);
-
     long endTime = System.nanoTime();
-    
+//output
     System.out.println(totalChar + " characters " + args[1].toCharArray()[0] +" : " + (endTime - startTime) + " NanoSeconds to compute");
 
   }
--- Question3.java
+++ Question3.java
@@ -1,5 +1,24 @@
 public class Question3{
+
+  public static int max(String[] intList, int currMax,int count){
+    if(count == (intList.length - 1)){
+      return currMax;
+    }else if (currMax < Integer.parseInt(intList[count])) {
+        currMax = Integer.parseInt(intList[count]);
+        count++;
+        max(intList,currMax,count);
+    }
+      count++;
+      return max(intList,currMax,count);
+  }
+
   public static void main(String[] args){
+
+    long startTime = System.nanoTime();
+    int Largest = max(args,Integer.parseInt(args[0]),0);
+    long endTime = System.nanoTime();
+
+    System.out.println( Largest + " is the largest number : " + (endTime - startTime) + " NanoSeconds to compute");
 
   }
 }
