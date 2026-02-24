0818dbfc2242d670cc188b4085d5b53525225a06
unknown <clydemail.inc@gmail.com>
2026-02-24 00:49:52 -0500

    finished question 1 with comments and completed question 2

--- Question1.java
+++ Question1.java
@@ -1,22 +1,25 @@
 
 public class Question1{
-//Recursive character length 
+//Recursive character length function
   public static int strLength(String inString){
+//This exits the program when there is no more characters left
     if(inString == null || inString.isEmpty() || inString.equals("") ){
+      //Returns zero so it doesn't add a character since there isnt one
       return 0;
     }
+//Recursively adds for each "loop" in recursion also reduces the string by one character
     return 1 + strLength(inString.substring(1));
   }
 
   public static void main(String[] args){
-
+//start time
     long startTime = System.nanoTime();
-
-    strLength(args[0]);
-
+//starts recursion
+    int totalChar = strLength(args[0]);
+//end time
     long endTime = System.nanoTime();
-
-    System.out.println(endTime-startTime + " NanoSeconds");
+//prints out length and find the value of time it took to compute
+    System.out.println(totalChar + " characters: " + (endTime - startTime) + " NanoSeconds to compute");
 
   }
 }
--- Question2.java
+++ Question2.java
@@ -1,5 +1,29 @@
+
 public class Question2{
+
+  public static int strTraversal(String inString, char inChar){
+
+     if(inString == null || inString.isEmpty() || inString.equals("") ){
+
+      return 0;
+    }else if(inString.charAt(0) == inChar){
+      return 1 + strTraversal(inString.substring(1), inChar);
+    }
+    else{
+      return strTraversal(inString.substring(1), inChar);
+    }
+
+  }
+
   public static void main(String[] args){
+
+    long startTime = System.nanoTime();
+
+    int totalChar = strTraversal(args[0],args[1].toCharArray()[0]);
+
+    long endTime = System.nanoTime();
+    
+    System.out.println(totalChar + " characters " + args[1].toCharArray()[0] +" : " + (endTime - startTime) + " NanoSeconds to compute");
 
   }
 }
