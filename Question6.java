533c5b0e64061d8e61bf9b631a2f73038939ec07
unknown <clydemail.inc@gmail.com>
2026-02-24 04:17:17 -0500

    finished all questions and added comments, need to add examples to each main for testing and explaining/ calculate big O

--- Question6.java
+++ Question6.java
@@ -1,13 +1,29 @@
 public class Question6{
-  public static long sumEven(String[] intArray){
-    
+//converts ints in string array to int and uses modulous to check if even
+  public static long sumEven(String[] intArray, int count){
+  //kinda like a while loop that checks count to length
+    if(count == intArray.length -1){
+
+      // returns last value if even or returns zero ending the loop 
+      if (Integer.parseInt(intArray[count])  % 2 == 0) {
+        return Integer.parseInt(intArray[count]);    
+      }else{
+        return 0;
+      }
+//if not end of array it checks if even and adds that to the return of the next recursion else only calls next recursion
+    }else if (Integer.parseInt(intArray[count]) % 2 == 0) {
+      return Integer.parseInt(intArray[count]) + sumEven(intArray, count+1);    
+    }
+
+    return sumEven(intArray,count+1);
   }
   public static void main(String[] args){
+    //same set up 
     long startTime = System.nanoTime();
-    long evenSum = sumEven(args);
+    long evenSum = sumEven(args,0);
     long endTime = System.nanoTime();
 
-    System.out.println( args + " evens added is "+ evenSum +" it took : "+(endTime - startTime) + " NanoSeconds to compute");
+    System.out.println(" Evens added is "+ evenSum +" it took : "+(endTime - startTime) + " NanoSeconds to compute");
 
   }
 }
